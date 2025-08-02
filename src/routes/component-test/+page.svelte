daecd7f603403a2c8c6aefe668efd760bcaeb3ba
Jon Hogendorp <jonhoog@live.nl>
2025-08-02 12:12:04 +0200

    Refactor code structure for improved readability and maintainability

--- src/routes/component-test/+page.svelte
+++ src/routes/component-test/+page.svelte
@@ -2,7 +2,12 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
-<Button size="lg">Primary Button</Button>
+<div class="flex h-full flex-col items-center justify-center gap-4">
+	<Button size="lg">Primary Button</Button>
+</div>
 
 <style>
+	:global(.sveltekit-loading) {
+		display: none;
+	}
 </style>
